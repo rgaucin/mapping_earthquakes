a01a01eb1a4f32776ce786aa2d96c877b7941c36
Gabriel Gaucin <>
2021-09-12 19:34:46 -0500

    create single point marker

--- mapping_single_points/static/js/logic.js
+++ mapping_single_points/static/js/logic.js
@@ -1,10 +1,10 @@
 console.log("working");
 
 // create map object with a center and zoom level
-let map = L.map("mapid").setView([40.7, -94.5], 4);
+let map = L.map("mapid").setView([34.0522, -118.2437], 14);
 
 // tile layer for map background
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -12,3 +12,10 @@ let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // add "graymap" tile layer to map
 streets.addTo(map);
+
+// add map marker for Los Angeles, California
+L.circle([34.0522, -118.2437], {
+    radius:300,
+    color: "#000000",
+    fillColor: "#FFFFE0"
+}).addTo(map);
