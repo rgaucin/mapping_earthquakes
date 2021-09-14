08bd88e313123947568853498fb6f2268c4cf4b6
Gabriel Gaucin <>
2021-09-13 19:32:45 -0500

    map multiple lines

--- mapping_lines/static/js/logic.js
+++ mapping_lines/static/js/logic.js
@@ -1,10 +1,18 @@
 console.log("working");
 
 // create map object with a center and zoom level
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+let map = L.map("mapid").setView([37.6213, -122.3790], 5);
+
+// coordinates for line from LAX to SFO
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+];
 
 // tile layer for map background
-let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+let streets = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -26,3 +34,8 @@ cityData.forEach(function(city) {
         .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
         .addTo(map);
 });
+
+// create a polyline using line coordinates
+L.polyline(line, {
+    color: "yellow"
+}).addTo(map);
